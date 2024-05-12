cc5cd01f1d77810fab7f0e87c8feb420a2f62a5f
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2024-05-12 14:30:27 +0500

    feat: resolver standarts

---
+++ apps/nestar-api/src/libs/enums/commont.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+	SOMETHING_WENT_WRONG = 'Something went wrong',
+	NO_DATA_FOUND = 'No data found',
+	CREATED_FAILED = 'Creation failed',
+	UPDATED_FAILED = 'Updated failed',
+	REMOVE_FAILED = 'Remove failed',
+	UPLOAD_FAILED = 'Upload failed',
+	BAD_REQUEST = 'Bad request',
+
+	NO_MEMBER_NICK = 'No member nick',
+	BLOCKED_USER = 'You have been blocked',
+	WRONG_PASSWORD = 'Wrong password, try again',
+	NOT_AUTHENTICATED = ' You are not authenticated, please login first!',
+	TOKEN_NOT_EXIST = 'Bearer token is not provided',
+	ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles',
+	NOT_ALLOWED_REQEUST = 'Not Allowed Request',
+	PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg, or png file format',
+	SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied',
+}
